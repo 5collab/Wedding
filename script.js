5cbd8c3350761ec4dfd847c0bfc651f85bb5a288
5collab <ihabahammed5@gmail.com>
2026-07-30 23:35:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("load",()=>{
+
+setTimeout(()=>{
+
+document.getElementById("loader").style.display="none";
+
+},2000);
+
+});
+
+document.getElementById("openBtn").addEventListener("click",()=>{
+
+alert("Luxury invitation coming in next update ❤️");
+
+});
